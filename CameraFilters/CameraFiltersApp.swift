621ced3f3403c42e371c77382340a915d3434192
Kinzerskiy <kinzzersky@gmail.com>
2023-07-07 15:08:21 +0300

    Initial Commit

---
+++ CameraFilters/CameraFiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraFiltersApp.swift
+//  CameraFilters
+//
+//  Created by Mac Pro on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CameraFiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
